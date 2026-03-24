6748682ea8ff9d43e48993eb33b886bb747cd9d4
JuanBallesterosUwU <juandiegob6532@gmail.com>
2026-03-23 20:46:24 -0400

    feat: Add an Express server with a base setup.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+dotenv.config()
+connectDB()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API Hotel Trucks active' })
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`))
