6d15582a5a53faf3fced421d2a108886d19ba9b4
JuanBallesterosUwU <juandiegob6532@gmail.com>
2026-03-23 22:35:49 -0400

    feat: add Employee model with password encryption

---
+++ src/models/Employee.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const employeeSchema = new mongoose.Schema({
+    
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['worker', 'admin'], default: 'worker'},
+    active: { type: Boolean, default:true },
+
+}, { timestamps: true })
+
+employeeSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+employeeSchema.methods.comparePassword = function(password) {
+    return bcrypt.compare(password, this.password)
+}
+
+module.exports = mongoose.model('Employee', employeeSchema)
