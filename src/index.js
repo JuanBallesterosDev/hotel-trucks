dd9d2878174d26122d6562c8c54ed7c7874ec3a1
JuanBallesterosUwU <juandiegob6532@gmail.com>
2026-04-18 20:39:52 -0400

    feat: add shift controller and routes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const connectDB = require('./config/db')
+    const express = require('express')
+    const cors = require('cors')
+    const dotenv = require('dotenv')
+    const connectDB = require('./config/db')
 
-dotenv.config()
-connectDB()
+    dotenv.config()
+    connectDB()
 
-const app = express()
+    const app = express()
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/auth', require('./routes/auth.routes'))
-app.use('/api/rooms', require('./routes/room.routes'))
-app.use('/api/clients', require('./routes/client.routes'))
-app.use('/api/products', require('./routes/product.routes')) 
+    app.use(cors())
+    app.use(express.json())
+    app.use('/api/auth', require('./routes/auth.routes'))
+    app.use('/api/rooms', require('./routes/room.routes'))
+    app.use('/api/clients', require('./routes/client.routes'))
+    app.use('/api/products', require('./routes/product.routes')) 
+    app.use('/api/shifts', require('./routes/shift.routes'))
 
-app.get('/', (req, res) => {
-    res.json({ message: 'API Hotel Trucks active' })
-})
+    app.get('/', (req, res) => {
+        res.json({ message: 'API Hotel Trucks active' })
+    })
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
+    const PORT = process.env.PORT || 5000
+    app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
