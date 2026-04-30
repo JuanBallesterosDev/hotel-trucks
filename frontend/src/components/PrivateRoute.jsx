e6fce3d2238c4a99e638a3fc82ce1c1d596a467b
JuanBallesterosUwU <juandiegob6532@gmail.com>
2026-04-30 15:31:37 -0400

    fix: fixing problems with using useEffect on routing

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 
 const PrivateRoute = ({ children, role }) => {
-    const { employee, token } = useAuth()
+    const { employee, token, loading } = useAuth()
 
     if (loading) return <div>Cargando sesión...</div>;
     return employee ? children : <Navigate to="/login" />;
